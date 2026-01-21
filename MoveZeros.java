9b6240ad7fad14ae2e76e1b923900ead0b951d69
harini5608 <harinip536.l@gmail.com>
2026-01-21 22:02:17 +0530

    Moving Zeros at the end of array

---
+++ MoveZeros.java
@@ -0,0 +1,24 @@
+public class MoveZeros {
+    public static void movezeros(int arr[]){
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[index]=arr[i];
+                index++;
+                
+            }
+        }
+        while(index<arr.length){
+            arr[index]=0;
+            index++;
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={0,1,0,3,12};
+        movezeros(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
